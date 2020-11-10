465ac361c72274aeabf642d4fd3e34c2af33303c
MusaTamzid05 <tamzid101@gmail.com>
2020-11-11 00:50:54 +0600

    language count added.

--- codebaseLib/summarizer.go
+++ codebaseLib/summarizer.go
@@ -1,13 +1,18 @@
 package codebaseLib
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 type Summarizer struct {
-	path string
+	path       string
+	fileExtMap map[string]int
 }
 
 func NewSummarizer(path string) *Summarizer {
 	summarizer := Summarizer{path: path}
+	summarizer.fileExtMap = make(map[string]int)
 	return &summarizer
 }
 
@@ -20,6 +25,40 @@ func (s *Summarizer) Run() {
 	}
 
 	for _, path := range paths {
-		log.Println(path)
+		data := strings.Split(path, "/")
+		s.updateExts(data[len(data)-1])
 	}
+
+	s.showLanguage()
+}
+
+func (s *Summarizer) updateExts(name string) {
+
+	if !strings.Contains(name, ".") {
+		return
+	}
+
+	data := strings.Split(name, ".")
+	ext := data[len(data)-1]
+
+	if _, ok := s.fileExtMap[ext]; ok {
+		s.fileExtMap[ext] = s.fileExtMap[ext] + 1
+		return
+	}
+
+	s.fileExtMap[ext] = 0
+}
+
+func (s *Summarizer) showLanguage() {
+
+	languages := map[string]string{"py": "python", "cpp": "c++"}
+
+	for ext, count := range s.fileExtMap {
+		for languageExt, languageName := range languages {
+			if languageExt == ext {
+				log.Printf("%s = %d files.\n", languageName, count)
+			}
+		}
+	}
+
 }
